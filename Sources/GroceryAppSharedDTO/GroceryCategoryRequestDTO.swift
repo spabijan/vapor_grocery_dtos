ee74063b7abf31c20cf67c045e3d49a6e30c5af8
spabijan <pabi@toya.net.pl>
2025-04-02 12:47:11 +0200

    Add DTOs for GroceryCategory

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,16 @@
+//
+//  GroceryRequestDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by sebastian-mac-m2 on 02/04/2025.
+//
+
+public struct GroceryRequestDTO: Codable, Sendable {
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
